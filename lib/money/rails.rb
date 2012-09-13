64a74011190d614e80d20344529949524facea3c
Michał Kurek <oki@md6.org>
2012-09-13 14:01:38 +0200

    ::Money.default_currency

--- lib/money/rails.rb
+++ lib/money/rails.rb
@@ -21,7 +21,7 @@ module ActiveRecord #:nodoc:
             :class_name => 'Money',
             :mapping => mapping,
             :allow_nil => true,
-            :constructor => Proc.new { |cents, currency| ::Money.new(cents || 0, currency) },
+            :constructor => Proc.new { |cents, currency| ::Money.new(cents || 0, currency || ::Money.default_currency) },
             :converter => Proc.new { |value| value.respond_to?(:to_money) ? value.to_money : raise(ArgumentError, "Can't convert #{value.class} to Money") }
 
           define_method "#{name}_with_blank=" do |value|
